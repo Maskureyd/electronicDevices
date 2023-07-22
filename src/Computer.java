6c43284557a74b9283e88ecd1530c17421f6d213
Emre Kaya <emre9kaya@gmail.com>
2023-07-22 13:40:51 +0300

    Update Computer.java

--- src/Computer.java
+++ src/Computer.java
@@ -18,7 +18,7 @@ public class Computer extends ElectronicDevice {
     public void updateOperatingSystem(String newSystem) {
         if (isOpen()) {
             this.operatingSystem = newSystem;
-            System.out.println("Updating operating system...");
+            System.out.println("Updating operating system..");
             System.out.println("Operating system updated: " + this.operatingSystem);
         } else {
             throw new IllegalArgumentException("The device must be turned on to update the operating system.");
